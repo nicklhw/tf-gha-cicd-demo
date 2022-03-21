92cde1891a59da8a93bb2b162b50bd1a7027fe12
nicklhw <nicklhw@gmail.com>
2022-03-21 14:07:28 -0400

    update PR request to use repo token

--- main.tf
+++ main.tf
@@ -44,6 +44,7 @@ data "aws_ami" "ubuntu" {
 
 resource "aws_instance" "web" {
   ami                    = data.aws_ami.ubuntu.id
+
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
@@ -55,6 +56,10 @@ resource "aws_instance" "web" {
               echo "Hello World" > /var/www/html/index.html
               systemctl restart apache2
               EOF
+
+  tags = {
+    Name = "nwong-test"
+  }
 }
 
 resource "aws_security_group" "web-sg" {
